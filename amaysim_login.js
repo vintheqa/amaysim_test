6af42bc111d5cc69411b9b357c0e25f9f754ca29
vintheqa <49342367+vintheqa@users.noreply.github.com>
2019-04-06 21:46:26 +0800

    Add files via upload

---
+++ amaysim_login.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+describe('amaysim login', () => {
+    it('Checks logging in to amaysim account', () => {
+        browser.url('https://amaysim.com.au');
+        const loginBtn = $('=Login');
+		loginBtn.waitForExist(30000);
+		loginBtn.click();
+		const userName = $('#username');
+		const psWord = $('#password');
+		userName.waitForExist(30000);
+		psWord.waitForExist(30000);
+		userName.setValue('0468340754');
+		psWord.setValue('theHoff34');
+		const loginBtn2 = $('.arrow-next');
+		loginBtn2.click();
+		const loggedUser = $('.service-primary-heading');
+		loggedUser.waitForExist(30000);
+        assert.equal(loggedUser.getText(), "0468 340 754");
+    });
+});
