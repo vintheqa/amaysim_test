e3a0290d78093c6f58a584031716bf7c26100605
vintheqa <49342367+vintheqa@users.noreply.github.com>
2019-04-07 14:32:29 +0800

    added saving of screenshots

--- amaysim_login.js
+++ amaysim_login.js
@@ -1,21 +1,24 @@
 const assert = require('assert');
 
 describe('amaysim login', () => {
-    it('Tests logging in to an amaysim account', () => {
-        browser.url('https://amaysim.com.au');
-        const loginBtn = $('=Login');
-	loginBtn.waitForExist(30000);
-	loginBtn.click();
-	const userName = $('#username');
-	const psWord = $('#password');
-	userName.waitForExist(30000);
-	psWord.waitForExist(30000);
-	userName.setValue('0468340754');
-	psWord.setValue('theHoff34');
-	const loginBtn2 = $('.arrow-next');
-	loginBtn2.click();
-	const loggedUser = $('.service-primary-heading');
-	loggedUser.waitForExist(30000);
-        assert.equal(loggedUser.getText(), "0468 340 754");
+    it('Checks logging in to amaysim account', () => {
+        	browser.url('https://amaysim.com.au');
+       		const loginBtn = $('=Login');
+		loginBtn.waitForExist(30000);
+		browser.saveScreenshot('./Screenshots/001-Amaysim_login/screenshot01.png');
+		loginBtn.click();
+		const userName = $('#username');
+		const psWord = $('#password');
+		userName.waitForExist(30000);
+		psWord.waitForExist(30000);
+		userName.setValue('0468340754');
+		psWord.setValue('theHoff34');
+		browser.saveScreenshot('./Screenshots/001-Amaysim_login/screenshot02.png');
+		const loginBtn2 = $('.arrow-next');
+		loginBtn2.click();
+		const loggedUser = $('.service-primary-heading');
+		loggedUser.waitForExist(30000);
+        	assert.equal(loggedUser.getText(), "0468 340 754");
+		browser.saveScreenshot('./Screenshots/001-Amaysim_login/screenshot03.png')
     });
 });
